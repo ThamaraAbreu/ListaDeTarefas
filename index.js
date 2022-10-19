903cdb11baae88dac36648570188ebb3e7239430
ThamaraAbreu <abreuthamarar@gmail.com>
2022-10-19 17:31:18 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+let id = 0
+
+const tarefa = (id, novaTarefa) => `<div id=${'id'}>
+<p>${novaTarefa}</p>
+<input type="checkbox" />
+</div>`
+
+function adicionarTarefa(){
+    id++
+    const novaTarefa = document.getElementById('nome-tarefa'). value
+    document.querySelector('#lista-tarefas').innerHTML += tarefa (id, novaTarefa) 
+}
